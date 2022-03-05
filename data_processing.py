b4f606d9d874af2757bc059a51b6d650d33320fb
Gouthami567 <gouthami.chandraiahshetty@gmail.com>
2022-03-05 23:24:20 +0530

    added multiply function

--- data_processing.py
+++ data_processing.py
@@ -3,5 +3,9 @@ print("adding all the code related to data processing")
 def sum(a,b):
     return a+b
 
+def multiply(a,b):
+    return a+b
+
 
 print("added sum: ",sum(5,6))
+print("added multiply: ",sum(5,6))
